55dd81a4991b56d1d5b44753161c19d5b320be43
JoseMPT <s20030132@itsch.edu.mx>
2023-10-18 20:42:00 -0600

    Patch confusion...

--- app/src/main/java/mx/tecnm/cdhidalgo/testauth/MainActivity.kt
+++ app/src/main/java/mx/tecnm/cdhidalgo/testauth/MainActivity.kt
@@ -50,7 +50,7 @@ class MainActivity : AppCompatActivity() {
                     // Your server's client ID, not your Android client ID.
                     .setServerClientId(getString(R.string.web_client_id))
                     // Only show accounts previously used to sign in.
-                    .setFilterByAuthorizedAccounts(true)
+                    .setFilterByAuthorizedAccounts(false)
                     .build())
             // Automatically sign in when exactly one credential is retrieved.
             .setAutoSelectEnabled(true)
